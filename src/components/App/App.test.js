0b36b033e00feeab463aaca1b140ed9f670c5dc3
Piet2001 <piet2001@fireman.net>
2021-03-09 15:12:37 +0100

    Some tests to try

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,8 +1,32 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+// test if element exists
+test('renders Hello World', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Hello World/i);
-  expect(linkElement).toBeInTheDocument();
+  const textElement = screen.getByText(/Hello World/i);
+  expect(textElement).toBeInTheDocument();
+});
+
+// bolean test
+describe('true is truthy and false is falsy', () => {
+  test('true is truthy', () => {
+    expect(true).toBe(true);
+  });
+ 
+  test('false is falsy', () => {
+    expect(false).toBe(false);
+  });
+});
+
+test('3 is 3', () => {
+  expect(3).toBe(3);
+});
+
+test('text is text', () => {
+  expect('text').toBe('text');
+});
+
+test('text not is tekst', () => {
+  expect('text').not.toBe('tekst')
 });
