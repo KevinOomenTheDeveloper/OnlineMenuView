7550501b5ebd140a17ff518e30b390998ba72848
milan <439008@student.fontys.nl>
2021-03-16 15:34:38 +0100

    Add home component as example

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,6 @@
 import './App.sass';
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import Home from "../Home/Home";
 
 function App() {
   return (
@@ -8,6 +9,9 @@ function App() {
             <Switch>
                 <Route exact path='/'>
                     Hello World
+                </Route>
+                <Route exact path='/home'>
+                    <Home/>
                 </Route>
             </Switch>
         </Router>
