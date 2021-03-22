e7aa1a6059d42a2a0d9ba7ab9d44a1f98fbfb150
FontysMan <409255@student.fontys.nl>
2021-03-22 14:53:21 +0100

    global header and footer
    Created a global header and footer which can easelly be implemented in pages

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,6 +3,8 @@ import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import Home from "../Home/Home";
 import Example from "../Example/Example";
 import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+import Layout from "../Layout/Layout";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 
@@ -16,10 +18,15 @@ function App() {
                     <Example/>
                 </Route>
                 <Route exact path='/home'>
-                    <Home/>
+                    <Layout>
+                        <Home/>
+                    </Layout>
                 </Route>
                 <Route exact path='/header'>
                     <Header title="Pasta"/>
+                </Route>
+                <Route exact path='/footer'>
+                    <Footer/>
                 </Route>
             </Switch>
         </Router>
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./Footer.sass"
+import {Link} from "react-router-dom";
+
+const Footer = () => {
+    return (
+        <div className="wrapper">
+            <div className="Footer">
+                <p>Footer</p>
+            </div>
+        </div>
+    );
+};
+
+export default Footer;
