58d6b19805112f0a9801625703ee3773821abc41
Twan <piet2001@fireman.net>
2021-03-22 16:41:40 +0100

    FoodItems overview page

--- src/components/FoodItems/Category.js
+++ src/components/FoodItems/Category.js
@@ -4,7 +4,12 @@ import {Image} from 'react-bootstrap'
 function Category() {
     return (
         <div>
-            <Image src='https://images-ext-1.discordapp.net/external/l54ReplMW7jcDHDfF4-3CBqs3FvbJIhTaxFlxfGfbFA/%3Fcrop%3D1.00xw%3A0.753xh%3B0%2C0.0647xh%26resize%3D768%3A%2A/https/hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/beste-sushi-amsterdam-1566315870.jpg'/>            
+            <h6>Category Name</h6>
+            <Image 
+            src='https://images-ext-1.discordapp.net/external/l54ReplMW7jcDHDfF4-3CBqs3FvbJIhTaxFlxfGfbFA/%3Fcrop%3D1.00xw%3A0.753xh%3B0%2C0.0647xh%26resize%3D768%3A%2A/https/hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/beste-sushi-amsterdam-1566315870.jpg' 
+            fluid 
+            />  
+            <p>Category description</p>    
         </div>
     )
 }
--- src/components/FoodItems/FoodItems.js
+++ src/components/FoodItems/FoodItems.js
@@ -1,20 +1,75 @@
 import React from 'react'
 import Category from './Category'
-import {Col, Row} from 'react-bootstrap'
+import { Col, Row } from 'react-bootstrap'
+import Container from "react-bootstrap/container";
 
 function FoodItems() {
     return (
         <div>
             Food items
-            <Row>
-                <Col xs={6}>
-                     <Category />                     
-                </Col>
-                <Col xs={6}>
-                     <Category />                     
-                </Col>
-            </Row>
-            
+            <Container>
+                <Row>
+                    <Col xs={3}>
+                        <Category />
+                    </Col>
+                    <Col xs={6}>
+                        <Category />
+                    </Col>
+                    <Col xs={3}>
+                        <Category />
+                    </Col>
+                </Row>
+                <Row>
+                    <Col xs={4}>
+                        <Category />
+                    </Col>
+                    <Col xs={4}>
+                        <Category />
+                    </Col>
+                    <Col xs={4}>
+                        <Category />
+                    </Col>
+                </Row>
+                <Row>
+                    <Col xs={6}>
+                        <Category />
+                    </Col>
+                    <Col xs={6}>
+                        <Category />
+                    </Col>
+                </Row>
+                <Row>
+                    <Col xs={2}>
+                        <Category />
+                    </Col>
+                    <Col xs={2}>
+                        <Category />
+                    </Col>
+                    <Col xs={2}>
+                        <Category />
+                    </Col>
+                    <Col xs={2}>
+                        <Category />
+                    </Col>
+                    <Col xs={2}>
+                        <Category />
+                    </Col>
+                    <Col xs={2}>
+                        <Category />
+                    </Col>
+                </Row>
+                <Row>
+                    <Col xs={3}>
+                        <Category />
+                    </Col>
+                    <Col xs={6}>
+                        <Category />
+                    </Col>
+                    <Col xs={3}>
+                        <Category />
+                    </Col>
+                </Row>
+            </Container>
         </div>
     )
 }
