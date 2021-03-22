aabfa1e735dff5bb884d93d88292f3d98d2b039d
Twan <piet2001@fireman.net>
2021-03-22 16:54:24 +0100

    Reorder fooditems

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,47 +1,46 @@
 import './App.sass';
-import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "../Home/Home";
 import Example from "../Example/Example";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
 import Layout from "../Layout/Layout";
 import ShoppingCart from "../ShoppingCart/ShoppingCart";
+import FoodItems from "../FoodItems/FoodItems";
 import "bootstrap/dist/css/bootstrap.min.css";
-import FoodItems from "../FoodItems/FoodItems"
-
 
 function App() {
-  return (
-    <div className="App">
-        <Router>
-            <Switch>
-                <Route exact path='/'>
-                    Hello World
-                    <Example/>
-                </Route>
-                <Route exact path='/home'>
-                    <Layout>
-                        <Home/>
-                    </Layout>
-                </Route>
-                <Route exact path='/shoppingcart'>
-                    <Layout>
-                        <ShoppingCart/>
-                    </Layout>
-                </Route>
-                <Route exact path='/header'>
-                    <Header title="Pasta"/>
-                </Route>
-                <Route exact path='/footer'>
-                    <Footer/>
-                </Route>
-                <Route exact path='/FoodItems'>
-                    <FoodItems />
-                </Route>
-            </Switch>
-        </Router>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Router>
+                <Switch>
+                    <Route exact path='/'>
+                        Hello World
+                    <Example />
+                    </Route>
+                    <Route exact path='/home'>
+                        <Layout>
+                            <Home />
+                        </Layout>
+                    </Route>
+                    <Route exact path='/shoppingcart'>
+                        <Layout>
+                            <ShoppingCart />
+                        </Layout>
+                    </Route>
+                    <Route exact path='/header'>
+                        <Header title="Pasta" />
+                    </Route>
+                    <Route exact path='/FoodItems'>
+                        <FoodItems />
+                    </Route>
+                    <Route exact path='/footer'>
+                        <Footer />
+                    </Route>
+                </Switch>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
