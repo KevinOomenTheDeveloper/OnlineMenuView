4747fd77f403680d779aa0d4877e9246ac8c37e3
milan <439008@student.fontys.nl>
2021-03-22 15:56:44 +0100

    Add ShoppingCart.js component functionality

--- src/components/ShoppingCart/ShoppingCart.js
+++ src/components/ShoppingCart/ShoppingCart.js
@@ -4,9 +4,37 @@ import {Link} from "react-router-dom";
 import Container from "react-bootstrap/container";
 import Row from "react-bootstrap/row";
 import Col from "react-bootstrap/col";
-//import "bootstrap";
+import { useState} from "react";
 
 const ShoppingCart = () => {
+
+    const [products, setItems] = useState([
+        {
+            id: 1,
+            name: 'Tomato Soup',
+            price: "4.00",
+            description: "This is soup made with tomatoes"
+        },
+        {
+            id: 2,
+            name: "Salad",
+            price: "3.00",
+            description: "Salad with dressing"
+        },
+        {
+            id: 3,
+            name: "Steak",
+            price: "6.00",
+            description: "Lorem Ipsum"
+        },
+        {
+            id: 4,
+            name: "Salad",
+            price: "3.00",
+            description: "Salad with dressing"
+        }
+    ])
+
     return (
         <div className="wrapper">
 
@@ -14,46 +42,32 @@ const ShoppingCart = () => {
                 Back to hello world
             </Link>
             <Container>
+                {products.map((product) => (
+                    <div>
+                        <hr/>
+                        <Row>
+                            <Col>
+                                <h4>{product.name}</h4>
+                            </Col>
+                            <Col className="price">
+                                <h4>{product.price}</h4>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col>
+                                <h5>{product.description}</h5>
+                            </Col>
+                        </Row>
+                    </div>
+                ))}
                 <div>
-                    <hr></hr>
-                    <Row>
-                        <Col>
-                            <h4>TomatenSoep</h4>
-                        </Col>
-                        <Col className="price">
-                            <h4>4.00</h4>
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col>
-                            <h5>Dit is soep met tomaten erin.</h5>
-                        </Col>
-                    </Row>
-                </div>
-                <div>
-                    <hr></hr>
-                    <Row>
-                        <Col>
-                            <h4>Salade</h4>
-                        </Col>
-                        <Col className="price">
-                            <h4>3.00</h4>
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col>
-                            <h5>Salade met sla.</h5>
-                        </Col>
-                    </Row>
-                </div>
-                <div>
-                    <hr></hr>
+                    <hr/>
                     <Row>
                         <Col>
                             <h4>SubTotal</h4>
                         </Col>
                         <Col className="price">
-                            <h4>7.00</h4>
+                            <h4>{getPriceSum(products).toFixed(2)}</h4>
                         </Col>
                     </Row>
                     <Row>
@@ -69,7 +83,7 @@ const ShoppingCart = () => {
                             <h4>Total</h4>
                         </Col>
                         <Col className="price">
-                            <h4>7.00</h4>
+                            <h4>0.00</h4>
                         </Col>
                     </Row>
                 </div>
@@ -79,3 +93,11 @@ const ShoppingCart = () => {
 };
 
 export default ShoppingCart;
+
+function getPriceSum(products){
+    let total = 0;
+    for (let i = 0; i < products.length; i++){
+        total += parseFloat(products[i].price);
+    }
+    return total
+}
