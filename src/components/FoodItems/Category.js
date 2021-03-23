7fb287fbde92b52eff734cb2f76ae6117d5bac6c
Twan <piet2001@fireman.net>
2021-03-23 12:03:54 +0100

    default image link added

--- src/components/FoodItems/Category.js
+++ src/components/FoodItems/Category.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Image } from 'react-bootstrap'
 
-const Category = ({ Name }) => {
+const Category = ({ Name, ImageLink }) => {
     const imageClick = () => {
         alert(Name + ' CLICKED')
     }
@@ -10,14 +10,12 @@ const Category = ({ Name }) => {
         <div>
             <h6>{Name}</h6>
             <Image
-                src='https://images-ext-1.discordapp.net/external/l54ReplMW7jcDHDfF4-3CBqs3FvbJIhTaxFlxfGfbFA/%3Fcrop%3D1.00xw%3A0.753xh%3B0%2C0.0647xh%26resize%3D768%3A%2A/https/hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/beste-sushi-amsterdam-1566315870.jpg'
+                src={ImageLink}
                 fluid
                 onClick={imageClick}
             />
         </div>
     )
 }
-
-
 
 export default Category
--- src/components/FoodItems/FoodItems.js
+++ src/components/FoodItems/FoodItems.js
@@ -4,69 +4,51 @@ import { Col, Row } from 'react-bootstrap'
 import Container from "react-bootstrap/container";
 
 const FoodItems = () => {
+
+    let DefaultImage = 'https://images-ext-1.discordapp.net/external/l54ReplMW7jcDHDfF4-3CBqs3FvbJIhTaxFlxfGfbFA/%3Fcrop%3D1.00xw%3A0.753xh%3B0%2C0.0647xh%26resize%3D768%3A%2A/https/hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/beste-sushi-amsterdam-1566315870.jpg'
     return (
         <div>
             Food items
             <Container>
                 <Row>
                     <Col xs={3}>
-                        <Category Name='Pasta' />
+                        <Category Name='Pasta' ImageLink={DefaultImage} />
                     </Col>
                     <Col xs={6}>
-                        <Category Name='Vis' />
+                        <Category Name='Vis' ImageLink={DefaultImage} />
                     </Col>
                     <Col xs={3}>
-                        <Category />
+                        <Category Name='Soep' ImageLink={DefaultImage} />
                     </Col>
                 </Row>
                 <Row>
                     <Col>
-                        <Category />
+                        <Category Name='Vlees' ImageLink={DefaultImage} />
                     </Col>
                     <Col>
-                        <Category />
+                        <Category Name='IJs' ImageLink={DefaultImage} />
                     </Col>
                     <Col>
-                        <Category />
+                        <Category Name='Taart' ImageLink={DefaultImage} />
                     </Col>
                 </Row>
                 <Row>
                     <Col xs={6}>
-                        <Category />
+                        <Category Name='Pizza' ImageLink={DefaultImage} />
                     </Col>
                     <Col xs={6}>
-                        <Category />
-                    </Col>
-                </Row>
-                <Row>
-                    <Col xs={2}>
-                        <Category />
-                    </Col>
-                    <Col xs={2}>
-                        <Category />
-                    </Col>
-                    <Col xs={2}>
-                        <Category />
-                    </Col>
-                    <Col xs={2}>
-                        <Category />
-                    </Col>
-                    <Col xs={2}>
-                        <Category />
-                    </Col>
-                    <Col xs={2}>
-                        <Category />
+                        <Category Name='Friet' ImageLink={DefaultImage} />
                     </Col>
                 </Row>
                 <Row>
                     <Col xs={3}>
-                        <Category />
+                        <Category Name='Snacks' ImageLink={DefaultImage} />
                     </Col>
                     <Col xs={6}>
-                        <Category />
+                        <Category Name='Chinees' ImageLink={DefaultImage} />
                     </Col>
                     <Col xs={3}>
-                        <Category />
+                        <Category Name='Salade' ImageLink={DefaultImage} />
                     </Col>
                 </Row>
             </Container>
