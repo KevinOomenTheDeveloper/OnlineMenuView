2ece4e41d360abc58fd0255b03494da8426929bf
Twan <piet2001@fireman.net>
2021-03-23 09:56:03 +0100

    Make image clickable

--- src/components/FoodItems/Category.js
+++ src/components/FoodItems/Category.js
@@ -1,17 +1,24 @@
 import React from 'react'
-import {Image} from 'react-bootstrap'
+import { Image, Button } from 'react-bootstrap'
 
-function Category() {
+const Category = () => {
+    const imageClick = () => {
+        alert('CLICKED')
+    }
+
     return (
         <div>
-            <h6>Category Name</h6>
-            <Image 
-            src='https://images-ext-1.discordapp.net/external/l54ReplMW7jcDHDfF4-3CBqs3FvbJIhTaxFlxfGfbFA/%3Fcrop%3D1.00xw%3A0.753xh%3B0%2C0.0647xh%26resize%3D768%3A%2A/https/hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/beste-sushi-amsterdam-1566315870.jpg' 
-            fluid 
-            />  
-            <p>Category description</p>    
+            <h6>Name</h6>
+            <Image
+                src='https://images-ext-1.discordapp.net/external/l54ReplMW7jcDHDfF4-3CBqs3FvbJIhTaxFlxfGfbFA/%3Fcrop%3D1.00xw%3A0.753xh%3B0%2C0.0647xh%26resize%3D768%3A%2A/https/hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/beste-sushi-amsterdam-1566315870.jpg'
+                fluid
+                onClick={imageClick}
+            />
+            <p>Category description</p>
         </div>
     )
 }
+
+
 
 export default Category
