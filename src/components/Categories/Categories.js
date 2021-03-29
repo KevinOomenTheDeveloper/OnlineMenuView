f9bcbc7f8bc85672ce5ff8b7f192b835f5f913b8
Twan <piet2001@fireman.net>
2021-03-29 15:29:48 +0200

    Make category page list based

--- src/components/Categories/Categories.js
+++ src/components/Categories/Categories.js
@@ -1,54 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 import CategoriesItem from './CategoriesItem'
 import { Col, Row } from 'react-bootstrap'
 import Container from "react-bootstrap/container";
 
 const Categories = () => {
 
-    let DefaultImage = 'https://images-ext-1.discordapp.net/external/l54ReplMW7jcDHDfF4-3CBqs3FvbJIhTaxFlxfGfbFA/%3Fcrop%3D1.00xw%3A0.753xh%3B0%2C0.0647xh%26resize%3D768%3A%2A/https/hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/beste-sushi-amsterdam-1566315870.jpg'
+    //let DefaultImage = 'https://images-ext-1.discordapp.net/external/l54ReplMW7jcDHDfF4-3CBqs3FvbJIhTaxFlxfGfbFA/%3Fcrop%3D1.00xw%3A0.753xh%3B0%2C0.0647xh%26resize%3D768%3A%2A/https/hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/beste-sushi-amsterdam-1566315870.jpg'
+
+    const [categories, setItems] = useState([
+        {
+            id: 1,
+            name: 'Pasta',
+            image: 'https://media.discordapp.net/attachments/826071766807216128/826072364059852800/Z.png'
+        },
+        {
+            id: 2,
+            name: 'Vlees',
+            image: 'https://media.discordapp.net/attachments/826071766807216128/826071810126381116/9k.png'
+        },
+        {
+            id: 3,
+            name: 'Vis',
+            image: 'https://media.discordapp.net/attachments/826071766807216128/826072466295619604/9k.png'
+        },
+        {
+            id: 4,
+            name: 'Soep',
+            image: 'https://media.discordapp.net/attachments/826071766807216128/826072533803204628/Z.png'
+        }
+    ])
+
     return (
         <div>
-            <Container>
+            <Container >
                 <Row>
-                    <Col xs={3}>
-                        <CategoriesItem Name='Pasta' ImageLink={DefaultImage} />
+                {categories.map(categorie => (
+                    <Col className='Column' sm={4}>
+                        <CategoriesItem Name={categorie.name} ImageLink={categorie.image} />
                     </Col>
-                    <Col xs={6}>
-                        <CategoriesItem Name='Vis' ImageLink={DefaultImage} />
-                    </Col>
-                    <Col xs={3}>
-                        <CategoriesItem Name='Soep' ImageLink={DefaultImage} />
-                    </Col>
-                </Row>
-                <Row>
-                    <Col>
-                        <CategoriesItem Name='Vlees' ImageLink={DefaultImage} />
-                    </Col>
-                    <Col>
-                        <CategoriesItem Name='IJs' ImageLink={DefaultImage} />
-                    </Col>
-                    <Col>
-                        <CategoriesItem Name='Taart' ImageLink={DefaultImage} />
-                    </Col>
-                </Row>
-                <Row>
-                    <Col xs={6}>
-                        <CategoriesItem Name='Pizza' ImageLink={DefaultImage} />
-                    </Col>
-                    <Col xs={6}>
-                        <CategoriesItem Name='Friet' ImageLink={DefaultImage} />
-                    </Col>
-                </Row>
-                <Row>
-                    <Col xs={3}>
-                        <CategoriesItem Name='Snacks' ImageLink={DefaultImage} />
-                    </Col>
-                    <Col xs={6}>
-                        <CategoriesItem Name='Chinees' ImageLink={DefaultImage} />
-                    </Col>
-                    <Col xs={3}>
-                        <CategoriesItem Name='Salade' ImageLink={DefaultImage} />
-                    </Col>
+                ))}
                 </Row>
             </Container>
         </div>
