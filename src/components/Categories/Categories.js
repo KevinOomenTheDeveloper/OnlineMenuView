105dcf2e798545f8b448d410de7e81bb9c1c2608
Twan <piet2001@fireman.net>
2021-03-29 15:58:00 +0200

    remove old default link

--- src/components/Categories/Categories.js
+++ src/components/Categories/Categories.js
@@ -4,9 +4,6 @@ import { Col, Row } from 'react-bootstrap'
 import Container from "react-bootstrap/container";
 
 const Categories = () => {
-
-    //let DefaultImage = 'https://images-ext-1.discordapp.net/external/l54ReplMW7jcDHDfF4-3CBqs3FvbJIhTaxFlxfGfbFA/%3Fcrop%3D1.00xw%3A0.753xh%3B0%2C0.0647xh%26resize%3D768%3A%2A/https/hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/beste-sushi-amsterdam-1566315870.jpg'
-
     const [categories, setItems] = useState([
         {
             id: 1,
