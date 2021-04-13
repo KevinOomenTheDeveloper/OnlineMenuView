60ca6ac5dfc6865dfc6fc0404e85d97b7216d60a
DavidChang <57895305+Davidchang24@users.noreply.github.com>
2021-04-13 13:38:20 +0200

    added HTTP requests to categories

--- src/components/Categories/Categories.js
+++ src/components/Categories/Categories.js
@@ -1,44 +1,81 @@
-import React, { useState } from 'react'
-import CategoriesItem from './CategoriesItem'
-import { Col, Row, Container} from 'react-bootstrap'
+import React, { useState, useEffect } from "react";
+import CategoriesItem from "./CategoriesItem";
+import { Col, Row, Container } from "react-bootstrap";
+import axios from "axios";
 
 const Categories = () => {
-    const [categories, setItems] = useState([
-        {
-            id: 1,
-            name: 'Pasta',
-            image: 'https://media.discordapp.net/attachments/826071766807216128/826072364059852800/Z.png'
-        },
-        {
-            id: 2,
-            name: 'Vlees',
-            image: 'https://media.discordapp.net/attachments/826071766807216128/826071810126381116/9k.png'
-        },
-        {
-            id: 3,
-            name: 'Vis',
-            image: 'https://media.discordapp.net/attachments/826071766807216128/826072466295619604/9k.png'
-        },
-        {
-            id: 4,
-            name: 'Soep',
-            image: 'https://media.discordapp.net/attachments/826071766807216128/826072533803204628/Z.png'
-        }
-    ])
+  // get all categories
+  const [categories, setItems] = useState([]);
+  useEffect(() => {
+    axios
+      .get("http://localhost:9011/menu/categories")
+      .then((res) => {
+        console.log(res);
+        setCategories(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
 
-    return (
-        <div>
-            <Container >
-                <Row>
-                {categories.map(categorie => (
-                    <Col className='Column' sm={4}>
-                        <CategoriesItem Name={categorie.name} ImageLink={categorie.image} />
-                    </Col>
-                ))}
-                </Row>
-            </Container>
-        </div>
-    )
-}
+  // get category by ID
+  const [category, setItems] = useState({});
+  const [id, setId] = useState(1);
 
-export default Categories
+  useEffect(() => {
+    axios
+      .get(`http://localhost:9011/menu/categories/${id}`)
+      .then((res) => {
+        console.log(res);
+        setItems(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
+
+  // create category
+  useEffect(() => {
+    axios
+      .post("http://localhost:9011/menu/categories/create", data)
+      .then((res) => {
+        console.log(res);
+        console.log(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
+
+  // update category
+  useEffect(() => {
+    axios
+      .put("http://localhost:9011/menu/categories/update", data)
+      .then((res) => {
+        console.log(res);
+        console.log(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
+
+  return (
+    <div>
+      <Container>
+        <Row>
+          {categories.map((categorie) => (
+            <Col className="Column" sm={4}>
+              <CategoriesItem
+                Name={categorie.name}
+                ImageLink={categorie.image}
+              />
+            </Col>
+          ))}
+        </Row>
+      </Container>
+    </div>
+  );
+};
+
+export default Categories;
