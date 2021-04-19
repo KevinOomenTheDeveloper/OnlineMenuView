7d6f055894bed610c0470cde951d61b2052a8cd2
milan <439008@student.fontys.nl>
2021-04-19 15:16:32 +0200

    Remove unused stuff

--- src/components/Categories/Categories.js
+++ src/components/Categories/Categories.js
@@ -1,109 +1,107 @@
-import React, { useState, useEffect } from "react";
+import React, {useState, useEffect} from "react";
 import CategoriesItem from "./CategoriesItem";
-import { Col, Row, Container } from "react-bootstrap";
+import {Col, Row, Container} from "react-bootstrap";
 import axios from "axios";
 
 const Categories = () => {
 
-  const [categories1, setItems] = useState([
-    {
-      id: 1,
-      name: "Pasta",
-      image:
-        "https://media.discordapp.net/attachments/826071766807216128/826072364059852800/Z.png",
-    },
-    {
-      id: 2,
-      name: "Vlees",
-      image:
-        "https://media.discordapp.net/attachments/826071766807216128/826071810126381116/9k.png",
-    },
-    {
-      id: 3,
-      name: "Vis",
-      image:
-        "https://media.discordapp.net/attachments/826071766807216128/826072466295619604/9k.png",
-    },
-    {
-      id: 4,
-      name: "Soep",
-      image:
-        "https://media.discordapp.net/attachments/826071766807216128/826072533803204628/Z.png",
-    },
-  ]);
+    const [categories1, setItems] = useState([
+        {
+            id: 1,
+            name: "Pasta",
+            image:
+                "https://media.discordapp.net/attachments/826071766807216128/826072364059852800/Z.png",
+        },
+        {
+            id: 2,
+            name: "Vlees",
+            image:
+                "https://media.discordapp.net/attachments/826071766807216128/826071810126381116/9k.png",
+        },
+        {
+            id: 3,
+            name: "Vis",
+            image:
+                "https://media.discordapp.net/attachments/826071766807216128/826072466295619604/9k.png",
+        },
+        {
+            id: 4,
+            name: "Soep",
+            image:
+                "https://media.discordapp.net/attachments/826071766807216128/826072533803204628/Z.png",
+        },
+    ]);
 
-  // get all categories
-  const [categories, setCategories] = useState([]);
-  useEffect(() => {
-    axios
-      .get("http://localhost:9011/menu/categories")
-      .then((res) => {
-        console.log(res);
-        setCategories(res);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  });
+    // get all categories
+    const [categories, setCategories] = useState([]);
+    useEffect(() => {
+        axios
+            .get("http://localhost:9011/menu/categories")
+            .then((res) => {
+                console.log(res);
+                setCategories(res);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    });
 
-  // get category by ID
-  const [category, setCategory] = useState({});
-  const [id, setId] = useState(1);
+    // get category by ID
+    const [category, setCategory] = useState({});
+    const [id, setId] = useState(1);
 
-  useEffect(() => {
-    axios
-      .get(`http://localhost:9011/menu/categories/${id}`)
-      .then((res) => {
-        console.log(res);
-        setCategory(res);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  });
+    useEffect(() => {
+        axios
+            .get(`http://localhost:9011/menu/categories/${id}`)
+            .then((res) => {
+                console.log(res);
+                setCategory(res);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    });
 
-  // create category
-  useEffect(() => {
-    axios
-      .post("http://localhost:9011/menu/categories/create", category)
-      .then((res) => {
-        console.log(res);
-        console.log(category);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  });
+    // create category
+    useEffect(() => {
+        axios
+            .post("http://localhost:9011/menu/categories/create", category)
+            .then((res) => {
+                console.log(res);
+                console.log(category);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    });
 
-  // update category
-  useEffect(() => {
-    axios
-      .put("http://localhost:9011/menu/categories/update", category)
-      .then((res) => {
-        console.log(res);
-        console.log(category);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  });
+    // update category
+    useEffect(() => {
+        axios
+            .put("http://localhost:9011/menu/categories/update", category)
+            .then((res) => {
+                console.log(res);
+                console.log(category);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    });
 
-  return (
-    <div>
-      <Container>
-        <Row>
-          {categories1.map((categorie) => (
-            <Col className="Column" sm={4}>
-              <CategoriesItem
-                Name={categorie.name}
-                ImageLink={categorie.image}
-              />
-            </Col>
-          ))}
-        </Row>
-      </Container>
-    </div>
-  );
+    return (
+        <Container>
+            <Row>
+                {categories1.map((categorie) => (
+                    <Col className="Column" sm={4}>
+                        <CategoriesItem
+                            Name={categorie.name}
+                            ImageLink={categorie.image}
+                        />
+                    </Col>
+                ))}
+            </Row>
+        </Container>
+    );
 };
 
 export default Categories;
--- src/components/CreateFood/CreateFood.js
+++ src/components/CreateFood/CreateFood.js
@@ -1,6 +1,6 @@
 import ProductDetails from './ProductDetails';
 import ProductIngredients from './ProductIngredients'
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect} from 'react';
 import './CreateFood.css';
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/row";
@@ -8,11 +8,11 @@ import Col from "react-bootstrap/col";
 
 export default function CreateFood() {
 
-    const [text, setText] = useState ('')
-    const [description, setDescription] = useState ('')
-    const [price, setPrice] = useState ('')
-    const [allergy, setAllergy] = useState ('')
-    const [categorys, setCategory] = useState ([  {
+    const [text, setText] = useState('')
+    const [description, setDescription] = useState('')
+    const [price, setPrice] = useState('')
+    const [allergy, setAllergy] = useState('')
+    const [categorys, setCategory] = useState([{
         id: "1",
         value: "Pasta",
     },
@@ -34,19 +34,22 @@ export default function CreateFood() {
 
         <div>
             <div className="container">
-            <Row>
-                <Col>< ProductDetails /></Col>
-                <Col>< ProductIngredients /></Col>
-              </Row>
+                <Row>
+                    <Col>
+                        <ProductDetails/>
+                    </Col>
+                    <Col>
+                        <ProductIngredients/>
+                    </Col>
+                </Row>
 
 
                 <div className="buttonContainer">
                     <div className="divButtons">
-                        <button type="button" className="btn btn-primary" >Add</button>
+                        <button type="button" className="btn btn-primary">Add</button>
                         <button type="button" className="btn btn-light">Cancel</button>
                     </div>
                 </div>
-
 
 
             </div>
