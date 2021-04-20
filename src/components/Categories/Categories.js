3dee360bf70190f6f39314f33a9e99dd9d78caf6
Milan Koster van Groos <i439008@fhict.nl>
2021-04-20 12:36:20 +0200

    Fixed DishByCategories and Added Axios for pulling categories from database

--- src/components/Categories/Categories.js
+++ src/components/Categories/Categories.js
@@ -4,99 +4,30 @@ import {Col, Row, Container} from "react-bootstrap";
 import axios from "axios";
 import "./Category.sass"
 
-const Categories = () => {
+const CATEGORIES_API_REST_URL = "http://localhost:9191/menu/categories/all";
 
-    const [categories1, setItems] = useState([
-        {
-            id: 1,
-            name: "Pasta",
-            image:
-                "https://media.discordapp.net/attachments/826071766807216128/826072364059852800/Z.png",
-        },
-        {
-            id: 2,
-            name: "Vlees",
-            image:
-                "https://media.discordapp.net/attachments/826071766807216128/826071810126381116/9k.png",
-        },
-        {
-            id: 3,
-            name: "Vis",
-            image:
-                "https://media.discordapp.net/attachments/826071766807216128/826072466295619604/9k.png",
-        },
-        {
-            id: 4,
-            name: "Soep",
-            image:
-                "https://media.discordapp.net/attachments/826071766807216128/826072533803204628/Z.png",
-        },
-    ]);
+
+const Categories = () => {
 
     // get all categories
     const [categories, setCategories] = useState([]);
     useEffect(() => {
-        axios
-            .get("http://localhost:9011/menu/categories")
-            .then((res) => {
-                console.log(res);
-                setCategories(res);
-            })
-            .catch((err) => {
-                console.log(err);
-            });
-    });
-
-    // get category by ID
-    const [category, setCategory] = useState({});
-    const [id, setId] = useState(1);
-
-    useEffect(() => {
-        axios
-            .get(`http://localhost:9011/menu/categories/${id}`)
-            .then((res) => {
-                console.log(res);
-                setCategory(res);
-            })
-            .catch((err) => {
-                console.log(err);
-            });
-    });
-
-    // create category
-    useEffect(() => {
-        axios
-            .post("http://localhost:9011/menu/categories/create", category)
-            .then((res) => {
-                console.log(res);
-                console.log(category);
-            })
-            .catch((err) => {
-                console.log(err);
-            });
-    });
-
-    // update category
-    useEffect(() => {
-        axios
-            .put("http://localhost:9011/menu/categories/update", category)
-            .then((res) => {
-                console.log(res);
-                console.log(category);
-            })
-            .catch((err) => {
-                console.log(err);
-            });
-    });
+        const fetchCategories = async () => {
+            const result = await axios(CATEGORIES_API_REST_URL);
+            console.log(result.data)
+            return result.data;
+        }
+        fetchCategories().then(r => setCategories(r));
+    }, []);
 
     return (
         <Container>
             <Row>
-                {categories1.map((categorie) => (
+                {categories.map((category) => (
                     <Col className="categories-column" sm={4}>
                         <CategoriesItem
-                            Name={categorie.name}
-                            ImageLink={categorie.image}
+                            Name={category.name}
+                            ImageLink={category.imageUrl}
                         />
                     </Col>
                 ))}
