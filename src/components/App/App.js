97bbd5751f4fdb57116060a2160188b3a713cef9
DavidChang <57895305+Davidchang24@users.noreply.github.com>
2021-04-26 09:46:31 +0200

    added axios ordering food post request

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,72 +1,66 @@
-import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Layout from "../Layout/Layout";
 import ShoppingCart from "../ShoppingCart/ShoppingCart";
 import Categories from "../Categories/Categories";
 import "bootstrap/dist/css/bootstrap.min.css";
-import React, {useState} from "react";
-import DishesByCategory from "../DishesByCategory/DishesByCategory"
-import "./App.sass"
+import React, { useState } from "react";
+import DishesByCategory from "../DishesByCategory/DishesByCategory";
+import "./App.sass";
 
 function App() {
-    const [tipTotal, setTipTotal] = useState(0);
+  const [products, setItems] = useState([
+    {
+      id: 1,
+      name: "Tomato Soup",
+      price: "4.00",
+      description: "This is soup made with tomatoes",
+    },
+    {
+      id: 2,
+      name: "Salad",
+      price: "3.00",
+      description: "Salad with dressing",
+    },
+    {
+      id: 3,
+      name: "Steak",
+      price: "6.00",
+      description: "Lorem Ipsum",
+    },
+    {
+      id: 4,
+      name: "Soup of the day",
+      price: "3.50",
+      description: "Ask the waiter for more information",
+    },
+  ]);
 
-    const [products, setItems] = useState([
-        {
-            id: 1,
-            name: "Tomato Soup",
-            price: "4.00",
-            description: "This is soup made with tomatoes",
-        },
-        {
-            id: 2,
-            name: "Salad",
-            price: "3.00",
-            description: "Salad with dressing",
-        },
-        {
-            id: 3,
-            name: "Steak",
-            price: "6.00",
-            description: "Lorem Ipsum",
-        },
-        {
-            id: 4,
-            name: "Soup of the day",
-            price: "3.50",
-            description: "Ask the waiter for more information",
-        },
-    ]);
-
-    return (
-        <Router>
-            <Switch>
-                <Route exact path='/'>
-                    <Layout title="Categories">
-                        <Categories products={products}/>
-                    </Layout>
-                </Route>
-                <Route exact path="/checkout">
-                    <Layout title="Shopping Cart">
-                        <ShoppingCart
-                            products={products}
-                            tipTotal={tipTotal}
-                            setTipTotal={setTipTotal}
-                        />
-                    </Layout>
-                </Route>
-                <Route exact path='/categories'>
-                    <Layout title="Categories">
-                        <Categories products={products}/>
-                    </Layout>
-                </Route>
-                <Route exact path='/dishesbycategory'>
-                    <Layout title="DishesByCategory">
-                        <DishesByCategory/>
-                    </Layout>
-                </Route>
-            </Switch>
-        </Router>
-    );
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Layout title="Categories">
+            <Categories products={products} />
+          </Layout>
+        </Route>
+        <Route exact path="/checkout">
+          <Layout title="Shopping Cart">
+            <ShoppingCart products={products} />
+          </Layout>
+        </Route>
+        <Route exact path="/categories">
+          <Layout title="Categories">
+            <Categories products={products} />
+          </Layout>
+        </Route>
+        <Route exact path="/dishesbycategory">
+          <Layout title="DishesByCategory">
+            <DishesByCategory />
+          </Layout>
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
--- src/components/ShoppingCart/ShoppingCart.js
+++ src/components/ShoppingCart/ShoppingCart.js
@@ -1,12 +1,52 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./ShoppingCart.sass";
 import Container from "react-bootstrap/container";
 import Row from "react-bootstrap/row";
 import Col from "react-bootstrap/col";
 import ShoppingCartItem from "./ShoppingCartItem/ShoppingCartItem";
+import Button from "react-bootstrap/Button";
 import Tips from "./Tips/Tips";
+import axios from "axios";
 
-const ShoppingCart = ({ products, tipTotal, setTipTotal }) => {
+const ShoppingCart = ({ products }) => {
+  const initialState = {
+    id: null,
+    orderId: null,
+    dateTime: "",
+    tableId: null,
+    tip: null,
+    totalPrice: null,
+  };
+  const [tipTotal, setTipTotal] = useState(0);
+  const [order, setOrder] = useState(initialState);
+  const [submitted, setSubmitted] = useState(false);
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setOrder({ ...order, [name]: value });
+  };
+
+  const saveOrder = () => {
+    var data = {
+      tip: order.tip,
+      totalPrice: getPriceSum,
+    };
+
+    axios
+      .post("http://localhost:9191/orders/create", data)
+      .then((res) => {
+        setOrder({
+          tip: res.data.title,
+          totalPrice: res.data.totalPrice,
+        });
+        setSubmitted(true);
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return (
     <div className="shoppingcart-wrapper">
       <Container>
@@ -39,6 +79,9 @@ const ShoppingCart = ({ products, tipTotal, setTipTotal }) => {
               <h4>{getPriceSum(products, tipTotal).toFixed(2)}</h4>
             </Col>
           </Row>
+          <Button className="orderButton" onClick={saveOrder}>
+            Order!
+          </Button>
         </div>
       </Container>
     </div>
